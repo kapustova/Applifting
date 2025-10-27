cfa6cccb3ea8b4532b845701b6507b0de6f26e2d
Kapustova <Kapustova@cngroup.dk>
2025-10-27 17:01:08 +0100

    Rename and add third test

--- cypress/e2e/Applifting.js
+++ cypress/e2e/Applifting.js
@@ -2,12 +2,12 @@
 
 import { finishShopping } from "../page-objects/finishShopping"
 import { items } from "../fixtures/items.json"
-import { checkCart, itemView, searchItem } from "../page-objects/searchItem"     
+import { checkCart, itemView, searchItem } from "../page-objects/GetMethods"     
 import { selectors } from "../selectors/selectors"
-import { checkout } from "../page-objects/searchItem"
-import { searchItem } from "../page-objects/searchItem"
-import { checkCart } from "../page-objects/searchItem"
-import { itemView } from "../page-objects/searchItem"
+import { checkout } from "../page-objects/GetMethods"
+import { searchItem } from "../page-objects/GetMethods"
+import { checkCart } from "../page-objects/GetMethods"
+import { itemView } from "../page-objects/GetMethods"
 
 describe('Second part of my work', () => {
   beforeEach('Open test aplication', () => {
@@ -126,7 +126,83 @@ describe('Second part of my work', () => {
         checkCart.checkCart(items.item1.name, items.item1.id, items.item1.value, items.item1.price,2)     
         }) 
 
+
+     it ('Update cart - remove Item', () => {
+        //check text header of first page
+        cy.get(selectors.headerTitle).should('have.text', 'Welcome to my shop!')
+       
+        //search for ArticleNumber1
+        searchItem.searchItem(items.item1.id)  
         
+        //assertion article name is as expected
+        cy.get(selectors.productId(items.item1.id)).should('exist')
+        //click on ArticleName1
+        cy.get(selectors.productId(items.item1.id)).click()
+        
+        //check item view
+        itemView.itemView(items.item1.name, items.item1.id)
+        
+        //Add to cart
+        cy.get(selectors.addToCartBtn).click()
+        cy.get(selectors.cartCounter).should('have.text', '1')  
+
+        //search for ArticleNumber2
+        searchItem.searchItem(items.item2.id)  
+        
+        //assertion article name is as expected
+        cy.get(selectors.productId(items.item2.id)).should('exist')
+        //click on ArticleName1
+        cy.get(selectors.productId(items.item2.id)).click()
+        
+        //check item view
+        itemView.itemView(items.item2.name, items.item2.id)
+        
+        //Add to cart
+        cy.get(selectors.addToCartBtn).click()
+        cy.get(selectors.cartCounter).should('have.text', '2')  
+
+        //go to cart and verify items in the cart
+        checkCart.checkCart(items.item1.name, items.item1.id, items.item1.value, items.item1.price,1)
+        checkCart.checkCart(items.item2.name, items.item2.id, items.item2.value, items.item2.price,1)     
+        }) 
+
+        it ('Add to cart - one article twice', () => {
+        //check text header of first page
+        cy.get(selectors.headerTitle).should('have.text', 'Welcome to my shop!')
+       
+        //search for ArticleNumber1
+        searchItem.searchItem(items.item1.id)  
+        
+        //assertion article name is as expected
+        cy.get(selectors.productId(items.item1.id)).should('exist')
+        //click on ArticleName1
+        cy.get(selectors.productId(items.item1.id)).click()
+        
+        //check item view
+        itemView.itemView(items.item1.name, items.item1.id)
+        
+        //Add to cart
+        cy.get(selectors.addToCartBtn).click()
+        cy.get(selectors.cartCounter).should('have.text', '1')  
+
+        //search for ArticleNumber1
+        searchItem.searchItem(items.item1.id)  
+        
+        //assertion article name is as expected
+        cy.get(selectors.productId(items.item1.id)).should('exist')
+        //click on ArticleName1
+        cy.get(selectors.productId(items.item1.id)).click()
+        
+        //check item view
+        itemView.itemView(items.item1.name, items.item1.id)
+        
+        //Add to cart
+        cy.get(selectors.addToCartBtn).click()
+        cy.get(selectors.cartCounter).should('have.text', '1')  
+
+        //go to cart and verify items in the cart
+        checkCart.checkCart(items.item1.name, items.item1.id, items.item1.value, items.item1.price,2)     
+        })    
 
 
 
